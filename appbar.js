a7feeb4995646f5efbe6e792b437ec0ce21b6dfc
Isaac Sukin <ice@icecreamyou.com>
2009-08-09 03:10:30 +0000

    Several CSS and JS upgrades. Fixed some minor bugs. Added the appbar_defaults module!

--- appbar.js
+++ appbar.js
@@ -1,10 +1,15 @@
 // $Id$
 Drupal.behaviors.appbar = function (context) {
+  $('body').addClass('with-appbar');
   $('#appbar_container').show();
-  var interval = setInterval("$('#appbar_count').load('/appbar/refresh/count')", Drupal.settings.appbar.delay);
+  var interval = setInterval("appbar_refresh();", Drupal.settings.appbar.delay);
   $('#appbar_alerts').click(function() {
     $('#appbar_alerts_list').toggle();
     $('#appbar_alerts_list').load('/appbar/refresh/list');
     $('#appbar_count').load('/appbar/refresh/count');
   });
+}
+function appbar_refresh() {
+  $('#appbar_count').load('/appbar/refresh/count');
+  $('#appbar_messages').toggleClass('appbar-has-new', parseInt($('#appbar_count').html()) > 0);
 }
