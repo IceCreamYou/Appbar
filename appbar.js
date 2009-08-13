f834b3c59bb820e5b81d01e9d4c36945e4f75617
Isaac Sukin <ice@icecreamyou.com>
2009-08-13 20:39:44 +0000

    Reverting #544404 because it really belongs in im.module instead

--- appbar.js
+++ appbar.js
@@ -4,14 +4,9 @@ Drupal.behaviors.appbar = function (context) {
   $('#appbar_container').show();
   var interval = setInterval("appbar_refresh();", Drupal.settings.appbar.delay);
   $('#appbar_alerts').click(function() {
-    $('#appbar_im_console').hide();
     $('#appbar_alerts_list').toggle();
     $('#appbar_alerts_list').load('/appbar/refresh/list');
     $('#appbar_count').load('/appbar/refresh/count');
-  });
-  $('#appbar_im_text').click(function() {
-    $('#appbar_alerts_list').hide();
-    $('#appbar_im_console').toggle();
   });
 }
 function appbar_refresh() {
