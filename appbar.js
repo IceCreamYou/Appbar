4a28eb6e4a4033fec6739c9dcb4cf751094bbb6b
Isaac Sukin <ice@icecreamyou.com>
2009-09-13 05:04:24 +0000

    Hopefully resolving #538648 Alerts not showing in Alert Box

--- appbar.js
+++ appbar.js
@@ -5,11 +5,11 @@ Drupal.behaviors.appbar = function (context) {
   var interval = setInterval("appbar_refresh();", Drupal.settings.appbar.delay);
   $('#appbar_alerts').click(function() {
     $('#appbar_alerts_list').toggle();
-    $('#appbar_alerts_list').load('/appbar/refresh/list');
-    $('#appbar_count').load('/appbar/refresh/count');
+    $('#appbar_alerts_list').load(Drupal.settings.appbar.base_path +'appbar/refresh/list');
+    $('#appbar_count').load(Drupal.settings.appbar.base_path +'appbar/refresh/count');
   });
 }
 function appbar_refresh() {
-  $('#appbar_count').load('/appbar/refresh/count');
-  $('#appbar_messages').toggleClass('appbar-has-new', parseInt($('#appbar_count').html()) > 0); /* Doesn't work. Always shows class. */
+  $('#appbar_count').load(Drupal.settings.appbar.base_path +'appbar/refresh/count');
+  //$('#appbar_messages').toggleClass('appbar-has-new', parseInt($('#appbar_count').html()) > 0); /* Doesn't work. Always shows class. */
 }
